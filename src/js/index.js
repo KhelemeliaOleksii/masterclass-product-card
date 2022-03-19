957126ee402e7b4f6e7e57715c550ab1b0eb313d
KhelemeliaOleksii <xvdm@ukr.net>
2022-03-19 02:35:29 +0200

    add comments

--- src/js/index.js
+++ src/js/index.js
@@ -6,29 +6,41 @@ const refs = {
 
 refs.card.addEventListener("click", tasselClickListener);
 function tasselClickListener (event) {
+    // destruction target from event
     const {target} = event;
-    console.log(event);
-    // console.log("target:", target);
 
+    //if click miss on link
     if (target.nodeName !== "A") {
         return;
     }
 
+    //if click is performed on active tassels
     if (target.classList.contains("tassels__item--active") ) {
         return;
     }
-    const activeTassel = document.querySelector(".tassels__item--active");
-    activeTassel.classList.remove("tassels__item--active");
+    
+    //find previous active tassel
+    // and remove class '.tassels__item--active'
+    const previousActiveTassel = document.querySelector(".tassels__item--active");
+    if (previousActiveTassel) {
+        previousActiveTassel.classList.remove("tassels__item--active");
+    }
+
+    //add to target class '.tassels__item--active'
     target.classList.add("tassels__item--active");
 
 
-    const activePaneNonActual = document.querySelector('.panes__item--active');
-    activePaneNonActual.classList.remove('panes__item--active');
+    //find previous active pane
+    // and remove class '.panes__item--active'
+    const previousActivePane = document.querySelector('.panes__item--active');
+    if (previousActivePane) {
+        previousActivePane.classList.remove('panes__item--active');
+    }
 
+    // get id from target
     const activeId = target.href.split("#")[1];
-    const paneActiveActual = document.querySelector(`#${activeId}`);
-    paneActiveActual.classList.add('panes__item--active');
-    // console.log("target id:", activeId);
-
-
+    // select target pane
+    // and  add class '.panes__item--active'
+    const paneTarget = document.querySelector(`#${activeId}`);
+    paneTarget.classList.add('panes__item--active');
 }
